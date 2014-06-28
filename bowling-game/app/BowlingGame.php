596864fce75c5dc9af34784c4c8b75e320dff6e8
Ray van B <deslaper@gmail.com>
2014-06-29 01:15:36 +0200

    Finished the kata

--- bowling-game/app/BowlingGame.php
+++ bowling-game/app/BowlingGame.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ERROR);
 class BowlingGame {
 
 	private $rolls = [];
@@ -19,18 +19,46 @@ class BowlingGame {
 		return $this->rolls[$firstInFrame] + $this->rolls[$firstInFrame + 1] == 10;
 	}
 
-//	public function score()
-//	{
-//		$score = 0;
-//		$rollsMax = count($this->rolls);
-//		$firstInFrame = 0;
-//
-//		for ($frame = 0; $frame < 10; $frame++)
-//		{
-//			if ($this->isStrike($firstInFrame))
-//			{
-//
-//			}
-//		}
-//	}
+	public function score()
+	{
+		$score = 0;
+		$firstInFrame = 0;
+
+		for ($frame = 0; $frame < 10; $frame++)
+		{
+			if ($this->isStrike($firstInFrame))
+			{
+				$score += 10 + $this->nextTwoBallsForStrike($firstInFrame);
+				$firstInFrame++;
+			}
+			elseif ($this->isSpare($firstInFrame))
+			{
+				$score += 10 + $this->nextBallForSpare($firstInFrame);
+				$firstInFrame += 2;
+			}
+			else
+			{
+				$score += $this->twoBallsInFrame($firstInFrame);
+				$firstInFrame += 2;
+			}
+		}
+
+		return $score;
+	}
+
+	public function nextTwoBallsForStrike($firstInFrame)
+	{
+		return $this->rolls[$firstInFrame + 1] + $this->rolls[$firstInFrame + 2];
+	}
+
+	public function nextBallForSpare($firstInFrame)
+	{
+		return $this->rolls[$firstInFrame + 2];
+	}
+
+	public function twoBallsInFrame($firstInFrame)
+	{
+		return $this->rolls[$firstInFrame] + $this->rolls[$firstInFrame + 1];
+	}
+
 } 
--- bowling-game/tests/BowlingGameTest.php
+++ bowling-game/tests/BowlingGameTest.php
@@ -41,7 +41,27 @@ class BowlingGameTest extends PHPUnit_Framework_TestCase {
 	public function Should_role_gutter_game()
 	{
 		$this->roll_multiple_times(20, 0);
-//		$this->assertEquals(0, $this->g->score());
+		$this->assertEquals(0, $this->g->score());
+	}
+
+	/** @test */
+	public function should_role_perfect_game()
+	{
+		$this->roll_multiple_times(12, 10);
+		$this->assertEquals(300, $this->g->score());
+	}
+
+	/** @test */
+	public function should_role_turkey()
+	{
+		$this->roll_multiple_times(3, 10);
+		var_dump($this->g->score); die;
+//		$this->assertEquals(TRUE, );
+	}
+
+	public function roll_spare()
+	{
+		return $this->roll_multiple_times(2, 5);
 	}
 
 
